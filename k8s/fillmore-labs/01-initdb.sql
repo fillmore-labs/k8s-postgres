124a1bfbc7f2533b8271e03f5897017748494b2b
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2023-08-10 15:53:06 +0200

    Initial commit 🥳
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

---
+++ k8s/fillmore-labs/01-initdb.sql
@@ -0,0 +1,11 @@
+--
+CREATE ROLE "user" WITH
+  LOGIN
+  PASSWORD 'SCRAM-SHA-256$4096:9XPiuCrJgwYJQMr0KYOBzg==$pK3x/J7wxPuXA+3/G2UlApZy42ZH9cFDmK2xxI+fzpE=:EKbQtdsC6gMfZN3HNV1QTUWxn0WtEpdqlgEa/i6Vh8c=';
+
+CREATE DATABASE "database" WITH
+    OWNER "user";
+
+COMMENT ON
+    DATABASE "database"
+    IS 'sample database for name service';
